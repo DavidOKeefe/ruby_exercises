349929be82f1f34730b50f43d74fee18c78293c8
David OKeefe <davidpokeefe3@gmail.com>
2015-07-06 20:22:03 -0400

    First Factorial Solution

---
+++ CoderByte/first_factorial.rb
@@ -0,0 +1,21 @@
+def FirstFactorial(num)
+  return num.downto(1).inject(:*)
+end
+
+
+
+require "minitest/spec"
+require "minitest/autorun"
+
+describe "FirstFactorial" do
+  it "returns the factorial of 4 as 24" do
+    FirstFactorial(4).must_equal 24
+  end
+
+  it "returns the factorial of 8 as 40320" do
+    FirstFactorial(8).must_equal 40320
+  end
+end
+
+
+
